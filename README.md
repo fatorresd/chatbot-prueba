add79a55ee6a88ae316880e5bca9f1b2a05c9a5a
fatorresd <ftorres2018@alu.uct.cl>
2025-11-19 22:58:31 -0300

    first commit: Login-chatBot

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dependencias
+
+- react-router-dom: Para manejar rutas (Login, Dashboard, etc.)
+- axios: Para hacer peticiones HTTP (simuladas de inicio)
+- TypeScript types: Para tipado en React Router
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Login Chatbot</h1>
+      {/* Aquí irán tus rutas o componentes de login */}
+    </div>
+  )
+}
+
+export default App
